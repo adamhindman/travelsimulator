3431eb43b939b7ebbef1a33c32f15ceaeaa3a055
Adam Hindman <adam@genui.co>
2021-12-28 19:39:09 -0800

    formatting lists

--- src/helpText.js
+++ src/helpText.js
@@ -4,6 +4,10 @@ export const helpText = `
   <p>Type "go" and then the name of a nearby country to travel there.</p>
   <p>Or click on a country's name in the list to make me type it for you.</p>
   <hr />
+  <h4>SAVING PROGRESS</h4>
+  <p>The game will try to save your progress as you go, however I am not a very good programmer, so it might get messed up, resulting in some errors. If they happens, you're fucked, and you should type "FORGET" to remove your saved game and start over.</p>
+  <p>If that happens, contact me on MySpace for a full refund!</p>
+  <hr/>
   <h4>COMMANDS</h4>
   <ul>
     <li><strong>GO [place]</strong>: Travel to a nearby place.</li>
