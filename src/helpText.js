85b6f15ca7fb485abc76265c9d764f2e5aa13baa
Adam Hindman <adam@genui.co>
2022-01-23 19:32:49 -0800

    tweaking font size

--- src/helpText.js
+++ src/helpText.js
@@ -5,7 +5,7 @@ export const helpText = `
     <li><strong>LOOK [thing]</strong>: Look at something, or just look around.</li>
     <li><strong>TEL [place]</strong>: Teleport to a place.</li>
     <li><strong>PASSPORT</strong>: Check what countries you've visited.
-    <li><strong>FORGET</strong>: Forget your current location, in case the save system gets messed up.
+    <li><strong>FORGET</strong>: Forget everything, in case the save system gets messed up.
   </ul>
   <p>After typing a command, you can press [TAB] to autocomplete it!</p>
 `;
