269d509d4e503d41b1d2554e294af7074b3cd3cd
Adam Hindman <adam@genui.co>
2022-02-02 17:47:33 -0800

    added help for inv

--- src/helpText.js
+++ src/helpText.js
@@ -4,8 +4,9 @@ export const helpText = `
     <li><strong>GO [place]</strong>: Travel to a nearby place.</li>
     <li><strong>LOOK [thing]</strong>: Look at something, or just look around.</li>
     <li><strong>TEL [place]</strong>: Teleport to a place.</li>
-    <li><strong>PASSPORT</strong>: Check what countries you've visited.
-    <li><strong>FORGET</strong>: Forget everything, in case the save system gets messed up.
+    <li><strong>PASSPORT</strong>: Check what countries you've visited.</li>
+    <li><strong>FORGET</strong>: Forget everything.</li>
+    <li><strong>INV</strong>: Check your inventory.</li>
   </ul>
   <p>After typing a command, you can press [TAB] to autocomplete it!</p>
 `;
