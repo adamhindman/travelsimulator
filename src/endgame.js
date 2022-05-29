844982b792d92db3a289012f2c1576c63b628c8d
Adam Hindman <adam@genui.co>
2022-05-29 11:35:17 -0700

    initial

--- src/endgame.js
+++ src/endgame.js
@@ -3,9 +3,9 @@ export let endgameRoom = {
   type: "excluded",
   neighbors: [],
   description: "\"... Is that all?\" </p><p>\"That's all,\" you say, with finality.</p><p> \"That's the story of how I visited every country, including the ones on Mars, and the fictional ones. It felt like a lot. Not enough for you two though, eh?\"</p><p>They both shake their heads. </p><p>The boy says: \"Where did you go after that? Did you meet any more ghosts?\"</p><p>The girl says: \"Did you go into any more caves? Did you solve any crimes?\"</p><p>You chuckle. With effort, you get out of your very comfortable chair. You find the old, time-worn key from the drawer in your desk, and hold it up for them to see. Their eyes widen.</p><p>\"Have I ever opened up the gallery for you children? No? Well, maybe it's time.\"</p><p>They follow you excitedly down the hall, to the tall double doors which have always been locked to them, and so have always held so much fascination for them. They are talking to each other, whispering something your old ears cannot quite make out.</p><p>\"I brought some things home with me from my adventures, and most of them are stored in here.\"</p><p>When you flick the lightswitch on, many rows of shelves and cabinets can be seen. The air is stale, and every surface is covered in a layer of dust. </p><p>\"It's been a long time since anyone came in here. Please, feel free to look around.\"",
-  // objects:[
-  //   {name:"", article:"", description:""},
-  //   {name:"", article:"", description:""},
-  //   {name:"", article:"", description:""}
-  // ],
+  objects:[
+    {name:"ARRAY OF UNEARTHLY POWER SOURCES", article:"An", description:""},
+    {name:"", article:"", description:""},
+    {name:"", article:"", description:""}
+  ],
 }
